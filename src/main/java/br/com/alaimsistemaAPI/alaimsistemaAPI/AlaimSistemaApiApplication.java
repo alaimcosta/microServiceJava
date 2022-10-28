39138d62d40a758758477fab8acf7cce3af31114
Alaim de Jesus <alaim.costa23@gmail.com>
2022-10-28 19:45:05 -0300

    Primeiro commit do sistema

---
+++ src/main/java/br/com/alaimsistemaAPI/alaimsistemaAPI/AlaimSistemaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alaimsistemaAPI.alaimsistemaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaimSistemaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaimSistemaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alaimsistemaAPI/alaimsistemaAPI/AlaimSistemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alaimsistemaAPI.alaimsistemaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaimSistemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
